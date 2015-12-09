3a8a12aac4c0f73ea617cd4c1d96e369ba814906
chetantarale <chetan_tarale@yahoo.com>
2015-12-09 14:56:22 +0530

    set JAVA_HOME & PATH

--- java/install.sh
+++ java/install.sh
@@ -9,9 +9,6 @@ apt-get install -y openjdk-6-jdk
 update-alternatives --set java /usr/lib/jvm/java-6-openjdk-amd64/jre/bin/java
 update-alternatives --set javac /usr/lib/jvm/java-6-openjdk-amd64/bin/javac
 update-alternatives --set javaws /usr/lib/jvm/java-6-openjdk-amd64/jre/bin/javaws
-echo 'export JAVA_HOME=/usr/lib/jvm/java-6-openjdk-amd64' >> $HOME/.bashrc
-echo 'export PATH=$PATH:/usr/lib/jvm/java-6-openjdk-amd64/jre/bin' >> $HOME/.bashrc
-
 
 echo "================ Installing oracle-java7-installer ================="
 echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
