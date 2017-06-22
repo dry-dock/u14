2268aab6a7530bb5a43a2e636a179578d6897d6f
Shrivara <shrivara.jois@gmail.com>
2017-06-22 16:50:57 +0530

    Pin versions of java apt packages installed

--- java/install.sh
+++ java/install.sh
@@ -1,26 +1,26 @@
 #!/bin/bash -e
 
 echo "================= Installing default-jdk & jre ==================="
-apt-get install default-jre
-apt-get install default-jdk
+apt-get install default-jre=2:1.7-51
+apt-get install default-jdk=2:1.7-51
 
 echo "================= Installing openjdk-8-jdk ==================="
 add-apt-repository -y ppa:openjdk-r/ppa
 apt-get update
-apt-get install -y openjdk-8-jdk
+apt-get install -y openjdk-8-jdk=8u111-b14-3~14.04.1
 update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
 update-alternatives --set javac /usr/lib/jvm/java-8-openjdk-amd64/bin/javac
-apt-get install --reinstall ca-certificates
+apt-get install --reinstall ca-certificates=20160104ubuntu0.14.04.1
 add-apt-repository ppa:maarten-fonville/ppa
 apt-get update
-apt-get install icedtea-8-plugin
+apt-get install icedtea-8-plugin=1.5.3-0ubuntu0.15.10.2~maarten0~trusty
 update-alternatives --set javaws /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/javaws
 
 echo "================ Installing oracle-java8-installer ================="
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
 add-apt-repository -y ppa:webupd8team/java
 apt-get update
-apt-get install -y oracle-java8-installer
+apt-get install -y oracle-java8-installer=8u131-1~webupd8~2
 update-alternatives --set java /usr/lib/jvm/java-8-oracle/jre/bin/java
 update-alternatives --set javac /usr/lib/jvm/java-8-oracle/bin/javac
 update-alternatives --set javaws /usr/lib/jvm/java-8-oracle/jre/bin/javaws
