1ffb9cad632ae86a428e7e65e66f59d775d529bb
Niranjan Godbole <niranjan92@users.noreply.github.com>
2017-12-12 16:48:11 +0530

    update node from 4.8.6 to 4.8.7

--- node/install.sh
+++ node/install.sh
@@ -9,9 +9,9 @@ export NVM_DIR="/root/.nvm"
 # add source of nvm to .bashrc - allows user to use nvm as a command
 echo "source ~/.nvm/nvm.sh" >> $HOME/.bashrc
 
-echo "================= Installing nodejs 4.8.6 ==================="
+echo "================= Installing nodejs 4.8.7 ==================="
 curl -sSL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-sudo apt-get install -y nodejs=4.8.6-1nodesource1
+sudo apt-get install -y nodejs=4.8.7-1nodesource1
 
 echo "================= Installing yarn 0.24.5 ==================="
 sudo apt-key adv --fetch-keys http://dl.yarnpkg.com/debian/pubkey.gpg
