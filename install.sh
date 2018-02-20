0c8564cbdc5d4c8d514b6d8ced3fdd921cecc47b
jatindogra <dogra.jatin5@gmail.com>
2018-02-20 16:32:41 +0530

    updates git version in base image u14

--- install.sh
+++ install.sh
@@ -53,7 +53,7 @@ pip install -q virtualenv==15.1.0
 echo "================= Installing Git ==================="
 add-apt-repository ppa:git-core/ppa -y
 apt-get update
-apt-get install -q -y git=1:2.16.1*
+apt-get install -q -y git=1:2.16.2*
 
 echo "================= Installing Git LFS ==================="
 curl -sS https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
