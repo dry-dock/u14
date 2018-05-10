c7cc076a6b18f8b38bc63d142f9d791d6f35cd2c
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-05-10 09:30:46 +0530

    removing the patch version for googlecloud

--- install.sh
+++ install.sh
@@ -83,7 +83,7 @@ echo "================= Adding gclould ============"
 CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
 curl -sS https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo apt-get update && sudo apt-get install google-cloud-sdk=200.0.0
+sudo apt-get update && sudo apt-get install google-cloud-sdk=200.0*
 
 echo "================= Adding kubectl 1.10.0 ==================="
 curl -sSLO https://storage.googleapis.com/kubernetes-release/release/v1.10.0/bin/linux/amd64/kubectl
