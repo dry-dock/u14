2f50d32f4d576e03098d9fc327b0444a8bc9823d
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-08-09 13:29:29 +0530

    Update install.sh

--- java/install.sh
+++ java/install.sh
@@ -8,21 +8,13 @@ echo "================= Installing openjdk-10-jdk ==================="
 add-apt-repository -y ppa:openjdk-r/ppa
 apt-get update
 apt-get install -y -q openjdk-10-jdk
-update-alternatives --set java /usr/lib/jvm/java-10-openjdk-amd64/jre/bin/java
-update-alternatives --set javac /usr/lib/jvm/java-10-openjdk-amd64/bin/javac
-apt-get install --reinstall ca-certificates=20170717*
-add-apt-repository ppa:maarten-fonville/ppa
-apt-get update
-apt-get install -q icedtea-10-plugin=1.5*
-update-alternatives --set javaws /usr/lib/jvm/java-10-openjdk-amd64/jre/bin/javaws
+
 
 echo "================ Installing oracle-java10-installer ================="
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
 add-apt-repository -y ppa:linuxuprising/java
 apt-get update
 apt-get install -q -y oracle-java10-installer
-update-alternatives --set java /usr/lib/jvm/java-10-oracle/jre/bin/java
-update-alternatives --set javac /usr/lib/jvm/java-10-oracle/bin/javac
-update-alternatives --set javaws /usr/lib/jvm/java-10-oracle/jre/bin/javaws
+sudo apt-get install oracle-java10-set-default
 echo 'export JAVA_HOME=/usr/lib/jvm/java-10-oracle' >> /etc/drydock/.env
 echo 'export PATH=$PATH:/usr/lib/jvm/java-10-oracle/jre/bin' >> /etc/drydock/.env
