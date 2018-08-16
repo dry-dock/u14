f912e96c5d57f39fec1651274c77bd5624d9b6f5
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-08-16 17:55:41 +0530

    Update install.sh

--- java/install.sh
+++ java/install.sh
@@ -5,9 +5,15 @@ apt-get install -q default-jre=2:1.7*
 apt-get install -q default-jdk=2:1.7*
 
 echo "================= Installing openjdk-10-jdk ==================="
-add-apt-repository -y ppa:openjdk-r/ppa
-apt-get update
-apt-get install -y -q openjdk-10-jdk
+mkdir -p /usr/lib/jvm && cd /usr/lib/jvm
+wget "https://download.java.net/java/GA/jdk10/10.0.2/19aef61b38124481863b1413dce1855f/13/openjdk-10.0.2_linux-x64_bin.tar.gz"
+tar -xzf openjdk-10.0.2_linux-x64_bin.tar.gz
+mv jdk-10.0.2/ java-10-openjdk-amd64
+
+sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/java-10-openjdk-amd64/bin/java 1
+sudo update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/java-10-openjdk-amd64/bin/javac 1
+sudo update-alternatives --set java /usr/lib/jvm/java-10-openjdk-amd64/bin/java
+sudo update-alternatives --set javac /usr/lib/jvm/java-10-openjdk-amd64/bin/javac
 
 
 echo "================ Installing oracle-java10-installer ================="
