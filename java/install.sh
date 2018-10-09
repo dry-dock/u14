86ef494f885aeb1feddf71d25bf39b02265af370
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-10-09 12:08:58 +0530

    update java latest version

--- java/install.sh
+++ java/install.sh
@@ -1,24 +1,21 @@
 #!/bin/bash -e
 
+echo "================ Installing openjdk11-installer ================="
+sudo add-apt-repository ppa:openjdk-r/ppa
+sudo apt-get update
+sudo apt install -y openjdk-11-jdk --allow-unauthenticated
 
-echo "================= Installing openjdk-10-jdk ==================="
-export OPENJDK_VERSION="10.0.2"
+echo "================ Installing oracle-java11-installer ================="
+export ORACLEJDK_VERSION=11
 mkdir -p /usr/lib/jvm && cd /usr/lib/jvm
-wget "https://download.java.net/java/GA/jdk10/$OPENJDK_VERSION/19aef61b38124481863b1413dce1855f/13/openjdk-"$OPENJDK_VERSION"_linux-x64_bin.tar.gz"
-tar -xzf openjdk-"$OPENJDK_VERSION"_linux-x64_bin.tar.gz
-mv jdk-"$OPENJDK_VERSION"/ java-10-openjdk-amd64
+wget --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/11+28/55eed80b163941c8885ad9298e6d786a/jdk-11_linux-x64_bin.tar.gz
+tar -xzf jdk-"$ORACLEJDK_VERSION"_linux-x64_bin.tar.gz
+mv jdk-"$ORACLEJDK_VERSION"/ java-11-oraclejdk-amd64
+sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/java-11-oraclejdk-amd64/bin/java 2
+sudo update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/java-11-oraclejdk-amd64/bin/javac 2
+sudo update-alternatives --set java /usr/lib/jvm/java-11-oraclejdk-amd64/bin/java
+sudo update-alternatives --set javac /usr/lib/jvm/java-11-oraclejdk-amd64/bin/javac
+echo 'export JAVA_HOME=/usr/lib/jvm/java-11-oraclejdk-amd64' >> /etc/drydock/.env
+echo 'export PATH=$PATH:/usr/lib/jvm/java-11-oraclejdk-amd64/bin' >> /etc/drydock/.env
 
-sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/java-10-openjdk-amd64/bin/java 1
-sudo update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/java-10-openjdk-amd64/bin/javac 1
-sudo update-alternatives --set java /usr/lib/jvm/java-10-openjdk-amd64/bin/java
-sudo update-alternatives --set javac /usr/lib/jvm/java-10-openjdk-amd64/bin/javac
 
-
-echo "================ Installing oracle-java10-installer ================="
-echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
-add-apt-repository -y ppa:linuxuprising/java
-apt-get update
-apt-get install -q -y oracle-java10-installer
-sudo apt-get install oracle-java10-set-default
-echo 'export JAVA_HOME=/usr/lib/jvm/java-10-oracle' >> /etc/drydock/.env
-echo 'export PATH=$PATH:/usr/lib/jvm/java-10-oracle/jre/bin' >> /etc/drydock/.env
