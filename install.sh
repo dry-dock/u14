ca8f5c705a44a80ccee1ab02a5f82066cf4dcd36
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2019-01-02 16:17:40 +0530

    update latest versions

--- install.sh
+++ install.sh
@@ -51,7 +51,7 @@ apt-get install  -y \
   python-software-properties=0.92* \
   python-dev=2.7*
 
-export VIRTUALENV_VERSION=16.0.0
+export VIRTUALENV_VERSION=16.2.0
 echo "================= Adding $VIRTUALENV_VERSION ==================="
 sudo pip install virtualenv=="$VIRTUALENV_VERSION"
 
@@ -63,7 +63,7 @@ apt-get install  -y git="$GIT_VERSION"
 
 
 # Git-LFS throws a warning that can be ignored - https://github.com/git-lfs/git-lfs/issues/2837
-export GIT_LFS=2.5.2
+export GIT_LFS=2.6.1
 echo "================= Installing Git LFS ==================="
 curl -sS https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 sudo apt-get install  git-lfs="$GIT_LFS"
@@ -82,26 +82,26 @@ echo "================= Installing Java  ==================="
 echo "================= Installing Ruby  ==================="
 . /u14/ruby/install.sh
 
-export GCLOUD_VERSION=222.0*
+export GCLOUD_VERSION=228.0*
 echo "================= Adding gcloud "$GCLOUD_VERSION"============"
 CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
 curl -sS https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-get update && sudo apt-get install google-cloud-sdk="$GCLOUD_VERSION"
 
-export KUBECTL_VERSION=v1.12.2
+export KUBECTL_VERSION=v1.13.1
 echo "================= Adding kubectl "$KUBECTL_VERSION" ==================="
 curl -sSLO https://storage.googleapis.com/kubernetes-release/release/"$KUBECTL_VERSION"/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
-KOPS_VERSION=1.10.0
+KOPS_VERSION=1.10.1
 echo "Installing KOPS version: $KOPS_VERSION"
 curl -LO https://github.com/kubernetes/kops/releases/download/"$KOPS_VERSION"/kops-linux-amd64
 chmod +x kops-linux-amd64
 mv kops-linux-amd64 /usr/local/bin/kops
 
-HELM_VERSION=v2.11.0
+HELM_VERSION=v2.12.1
 echo "Installing helm version: $HELM_VERSION"
 wget https://storage.googleapis.com/kubernetes-helm/helm-"$HELM_VERSION"-linux-amd64.tar.gz
 tar -zxvf helm-"$HELM_VERSION"-linux-amd64.tar.gz
@@ -109,17 +109,17 @@ mv linux-amd64/helm /usr/local/bin/helm
 rm -rf linux-amd64
 
 echo "================= Adding apache libcloud 2.3.0 ============"
-sudo pip install 'apache-libcloud==2.3.0'
+sudo pip install 'apache-libcloud==2.4.0'
 
-export AWS_VERSION=1.16.43
+export AWS_VERSION=1.16.81
 echo "================= Adding awscli "$AWS_VERSION" ============"
 sudo pip install  awscli=="$AWS_VERSION"
 
-export AWSEBCLI_VERSION=3.14.6
+export AWSEBCLI_VERSION=3.14.8
 echo "================= Adding awscli "$AWSEBCLI_VERSION" ============"
 sudo pip install  awsebcli=="$AWSEBCLI_VERSION"
 
-export OPENSTACKCLIENT_VERSION=3.16.1
+export OPENSTACKCLIENT_VERSION=3.17.0
 echo "================= Adding openstack client $OPENSTACKCLIENT_VERSION ============"
 sudo pip install python-openstackclient=="$OPENSTACKCLIENT_VERSION" --ignore-installed urllib3
 
@@ -136,7 +136,7 @@ sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893
 sudo apt-get install -q apt-transport-https=1.0.1*
 sudo apt-get update && sudo apt-get install -y -q azure-cli=$AZURE_CLI_VERSION
 
-export DOCTL_VERSION=1.11.0
+export DOCTL_VERSION=1.12.1
 echo "================= Adding doctl $DOCTL_VERSION============"
 curl -OL https://github.com/digitalocean/doctl/releases/download/v"$DOCTL_VERSION"/doctl-"$DOCTL_VERSION"-linux-amd64.tar.gz
 tar xf doctl-"$DOCTL_VERSION"-linux-amd64.tar.gz
@@ -144,13 +144,13 @@ sudo mv doctl /usr/local/bin
 rm doctl-"$DOCTL_VERSION"-linux-amd64.tar.gz
 
 
-export JFROG_VERSION=1.20.2
+export JFROG_VERSION=1.23.1
 echo "================= Adding jfrog-cli "$JFROG_VERSION"==================="
 wget -nv https://api.bintray.com/content/jfrog/jfrog-cli-go/"$JFROG_VERSION"/jfrog-cli-linux-amd64/jfrog?bt_package=jfrog-cli-linux-amd64 -O jfrog
 sudo chmod +x jfrog
 sudo mv jfrog /usr/bin/jfrog
 
-export ANSIBLE_VERSION=2.7.1
+export ANSIBLE_VERSION=2.7.5
 echo "================ Adding ansible $ANSIBLE_VERSION===================="
 sudo pip install ansible=="$ANSIBLE_VERSION"
 
@@ -158,7 +158,7 @@ export BOTO_VERSION=2.49.0
 echo "================ Adding boto $BOTO_VERSION ======================="
 sudo pip install  boto=="$BOTO_VERSION"
 
-export BOTO3_VERSION=1.9.33
+export BOTO3_VERSION=1.9.71
 echo "============  Adding boto3 "$BOTO_VERSION" ==============="
 sudo pip install boto3=="$BOTO3_VERSION"
 
@@ -170,7 +170,7 @@ export DOPY_VERSION=0.3.7
 echo "================ Adding dopy $DOPY_VERSION ======================="
 sudo pip install dopy=="$DOPY_VERSION"
 
-export TF_VERSION=0.11.10
+export TF_VERSION=0.11.11
 echo "================ Adding terraform-$TF_VERSION===================="
 export TF_FILE=terraform_"$TF_VERSION"_linux_amd64.zip
 
@@ -186,7 +186,7 @@ mv /tmp/terraform/terraform /usr/bin/terraform
 echo "Added terraform successfully"
 echo "-----------------------------------"
 
-export PK_VERSION=1.3.2
+export PK_VERSION=1.3.3
 echo "================ Adding packer $PK_VERSION ===================="
 export PK_FILE=packer_"$PK_VERSION"_linux_amd64.zip
 
